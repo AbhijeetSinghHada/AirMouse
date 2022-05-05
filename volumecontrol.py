2e314f13925e52069649ac8f669fa98a263d06f7
Abhijeet Singh Hada <abhi22hada@gmail.com>
2022-05-06 01:25:55 +0530

    commit

--- volumecontrol.py
+++ volumecontrol.py
@@ -1,5 +1,4 @@
 import cv2
-
 import time
 import numpy as np
 import HandTrainingModule as htm
@@ -22,11 +21,9 @@ while True:
     # }
     #     x1, y1 = lmList[4][1], lmList[4][2]
     #     x2, y2 = lmList[8][1], lmList[8][2]
-
     #     cv2.circle(img, (x1,y1), 15, (255, 0, 255), cv2.FILLED)
     #     cv2.circle(img, (x2,y2), 15, (255, 0, 255), cv2.FILLED)
     #     cv2.line(img, (x1,y1), (x2,y2), (255, 0, 255), 3)
-
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
