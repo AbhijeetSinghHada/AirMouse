3ac6732dc016c72238b05cc2f8981d3723086d14
Abhijeet Singh Hada <abhi22hada@gmail.com>
2022-05-06 01:38:38 +0530

    commit

--- volumecontrolnew.py
+++ volumecontrolnew.py
@@ -1,11 +1,11 @@
 import cv2
 import time
 import numpy as np
-import HandTrackingModule as htm
+import HandTrainingModule as htm
 import math
-from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from ctypes import cast, POINTER
  
 ################################
 wCam, hCam = 640, 480
@@ -19,8 +19,7 @@ pTime = 0
 detector = htm.handDetector(detectionCon=0.7)
  
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume.iid, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume.GetSpeakers, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
@@ -57,21 +56,18 @@ while True:
         volPer = np.interp(length, [50, 300], [0, 100])
         print(int(length), vol)
         volume.SetMasterVolumeLevel(vol, None)
- 
         if length < 50:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
  
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,1, (255, 0, 0), 3)
  
  
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,1, (255, 0, 0), 3)
  
     cv2.imshow("Img", img)
     cv2.waitKey(1)
